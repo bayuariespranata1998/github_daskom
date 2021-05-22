52bdece2a95e53355c52f6825392fe94a1cc2463
bayuariespranata1998 <bayuariespranata@gmail.com>
2021-05-22 17:27:44 +0700

    artikel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Home Page </title>
+<link rel="stylesheet" type="text/css" href="/CSS/main.css">
+</head>
+<body>
+<h1 align="center"> Halaman ini merupakann halaman Utama dari Web ( Home ) </h1>
+<center><a href="{{url('home\artikel')}}"> Artikel </a><br>
+<a href="{{url('home\ContactUs')}}"> Contact Us </a></center>
+
+</body>
+
+</html>
